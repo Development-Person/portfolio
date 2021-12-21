4887d5b42ab8e854064e512a045d857af6f5daec
Ibrahim Achkar <ibaemail@gmail.com>
2021-12-21 21:32:30 +1100

    implemented basic modal functionality

--- src/components/Dot.jsx
+++ src/components/Dot.jsx
@@ -1,9 +1,58 @@
+import { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import Modal from 'react-modal';
+
 function DotComponent(props) {
-  const onClick = () => {
-    console.log(props.info);
+  let subtitle;
+
+  const [modalIsOpen, setIsOpen] = useState(false);
+
+  const customStyles = {
+    content: {
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)',
+    },
   };
 
-  return <div onClick={onClick} className='dot' style={props.style}></div>;
+  useEffect(() => {
+    Modal.setAppElement('body');
+  }, []);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {
+    subtitle.style.color = '#f00';
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  const onClick = () => {
+    openModal();
+  };
+
+  return (
+    <>
+      <div onClick={onClick} className='dot' style={props.style}></div>
+      <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel='Project Modal'>
+        <h2 ref={(_subtitle) => (subtitle = _subtitle)}>{props.info}</h2>
+        <div>Some info about the project I made</div>
+        <button onClick={closeModal}>close</button>
+      </Modal>
+    </>
+  );
 }
 
 export default DotComponent;
