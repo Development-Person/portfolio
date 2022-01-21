d59ec4b4faaac713845fd3bc20dea812cf0adb10
Ibrahim Achkar <ibaemail@gmail.com>
2022-01-21 15:04:21 +1100

    updated project descriptions

--- src/content/project_descriptions.js
+++ src/content/project_descriptions.js
@@ -1,31 +1,123 @@
 const data = [
   {
-    name: 'pigeons',
+    name: 'ProShop',
+    date: 'November 2020',
     description:
-      'A simple website demonstrating use of modals and api calls to a publicly available api',
-    learnings: ['vanilla js', 'html', 'css', 'modals', 'fetch', 'public apis'],
-  },
-  {
-    name: 'set designer',
-    description:
-      'Playing around with soundfiles using vanilla javascript and html',
-    learnings: ['audio api', 'html', 'css', 'javascript', 'drag and drop'],
-  },
-  {
-    name: 'borrow yours',
-    description: 'Major project working with react, redux, express and mongoDB',
+      'Code along project to get me familiar with the MERN stack. Coded a working online store.',
     learnings: [
-      'react',
-      'redux (classic and redux toolkit)',
-      'nodejs & express',
       'mongoDB',
+      'React',
+      'Redux',
+      'Bootstrap',
+      'Express',
+      'Node',
+      'Building APIs',
+      'JSON',
+      'Github',
     ],
+    repo: ['https://github.com/Development-Person/ProShop'],
+    url: '',
   },
   {
-    name: 'excel file parser',
+    name: 'Borrow Yours',
+    date: 'December 2020',
+    description:
+      'Used what I learnt in Proshop and created a major project working with react, redux, express and mongoDB. BorrowYours allows friends to lend each other household items.',
+    learnings: [
+      'mongoDB',
+      'React',
+      'Redux Toolkit',
+      'Bootstrap',
+      'Express',
+      'Node',
+      'Building APIs',
+      'JSON',
+      'Github',
+    ],
+    repo: ['https://github.com/Development-Person/BorrowYours'],
+    url: '',
+  },
+  {
+    name: 'CSV Parser',
+    date: 'January 2021',
     description:
       'Working with sails, developed a backend utility to parse excel files and upload their contents to an SQL table',
-    learnings: ['sails', 'nodejs & express', 'file parsing', 'postgres/SQL'],
+    learnings: ['Sails', 'Express', 'Node', 'CSV file parsing', 'postgres/SQL'],
+    repo: ['https://github.com/Development-Person/csv-parser'],
+    url: '',
+  },
+  {
+    name: 'Set Designer',
+    date: 'February 2021',
+    description:
+      'Playing around with soundfiles using vanilla javascript and html',
+    learnings: ['Audio API', 'HTML', 'CSS', 'Vanilla JS'],
+    repo: ['https://github.com/Development-Person/SetDesigner'],
+    url: '',
+  },
+  {
+    name: 'Pigeons!',
+    date: 'May 2021',
+    description:
+      'A simple website demonstrating use of modals and api calls to a publicly available api',
+    learnings: [
+      'HTML',
+      'CSS',
+      'Vanilla JS',
+      'fetch function',
+      'querying public apis',
+    ],
+    repo: ['https://github.com/Development-Person/pigeons'],
+    url: '',
+  },
+  {
+    name: 'Cardano NFT Minter',
+    date: 'October 2021',
+    description:
+      'Created a system which monitors a Cardano address for transactions, verifies the transaction, mints and NFT and sends it back to the originating address. Created a front end advertising a unique NFT project I created in order to learn how to code programs which interact with the Cardano network',
+    learnings: ['HTML', 'CSS', 'Petite-Vue', 'Node', 'Firebase', 'Cardano'],
+    repo: [
+      'https://github.com/Development-Person/nft_minting_server',
+      'https://github.com/Development-Person/nft_firebase',
+    ],
+    url: 'https://treatdeliverysystem.com/',
+  },
+  {
+    name: 'Tweet Competition',
+    date: 'November 2021',
+    description:
+      "Built a full stack app to run twitter like and follow competitions. User enters a tweet URL, backend then queries the Twitter API for information about a tweet and selects a winner based on the user's selections.",
+    learnings: [
+      'HTML',
+      'CSS',
+      'Vanilla JS',
+      'fetch function',
+      'querying public apis',
+      'Express',
+      'Node',
+      'Heroku',
+    ],
+    repo: ['https://github.com/Development-Person/tweet_analyzer'],
+    url: 'https://tweetcomp.herokuapp.com/',
+  },
+  {
+    name: 'From the Ground Up',
+    date: 'May 2021',
+    description:
+      'My idea was selected as a group project in the Le Wagon coding bootcamp. From the Ground Up is a learning platform which allows users to create learning plans for anything they wish to learn, and connect with like minded people.',
+    learnings: [
+      'Ruby on Rails',
+      'SQL',
+      'HTML',
+      'CSS',
+      'Vanilla JS',
+      'Bootstrap',
+      'As de facto team leader, managing team workflow',
+      'Working in a team',
+      'Github',
+    ],
+    repo: ['https://github.com/Development-Person/from_the_ground_up'],
+    url: 'http://www.learnfromthegroundup.com/',
   },
 ];
 
