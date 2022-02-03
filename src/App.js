fb6326d7c7dc976311c48b19d1b0ca614f34714c
Ibrahim Achkar <ibaemail@gmail.com>
2022-02-03 11:55:57 +1100

    moved all grid logic into grid component and refined sizing for different screen sizes

--- src/App.js
+++ src/App.js
@@ -1,45 +1,12 @@
 import './App.css';
 import GridComponent from './components/Grid';
 import HeadingComponent from './components/Heading';
-import { getGridAllCoordinates } from './functions/gridPlacement';
-import { useEffect, useState } from 'react';
-import { data } from './content/project_descriptions';
 
 function App() {
-  const height = window.screen.availHeight - 200;
-  const width = window.screen.availWidth - 100;
-
-  const [coordinatesArray, setCoordinatesArray] = useState();
-
-  // projects data
-  const projectsArrayLength = data.length;
-
-  // determining number of rows based on screen size
-  const columnsDivisor = height > 740 ? 100 : 100;
-  const columns = Array.from(Array(Math.floor(height / columnsDivisor)).keys());
-  const rows = Array.from(Array(Math.floor(width / 100)).keys());
-
-  //placing all elements into grid on page load
-  useEffect(() => {
-    setCoordinatesArray(
-      getGridAllCoordinates(rows.length, columns.length, projectsArrayLength)
-    );
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
   return (
     <div className='App'>
       <HeadingComponent />
-      <div className='game' style={{ height: height + 50, width: width + 25 }}>
-        <GridComponent
-          coordinatesArray={coordinatesArray}
-          projectsArrayLength={projectsArrayLength}
-          rows={rows}
-          columns={columns}
-          width={width}
-          height={height}
-        />
-      </div>
+      <GridComponent />
     </div>
   );
 }
--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,40 +1,79 @@
 import { Container, Row, Col } from 'react-bootstrap';
 import DotComponent from './Dot';
+import { data } from '../content/project_descriptions';
+import { useEffect, useState } from 'react';
+import { getGridAllCoordinates } from '../functions/gridPlacement';
 
-function GridComponent(props) {
+function GridComponent() {
+  const screenWidth = window.screen.availWidth;
+  const screenHeight = window.screen.availHeight;
+
+  // projects data
+  const projectsArrayLength = data.length;
+
+  const gridWidth = screenWidth < 500 ? screenWidth - 50 : screenWidth - 200;
+  const gridHeight =
+    screenHeight < 700 ? screenHeight - 200 : screenHeight - 400;
+
+  const [coordinatesArray, setCoordinatesArray] = useState();
+
+  // determining number of rows based on screen size
+  const rowsDivisor = screenWidth >= 912 ? 120 : 100;
+  const columnsDivisor = screenHeight > 740 ? 120 : 100;
+  const columns = Array.from(
+    Array(Math.floor(gridHeight / columnsDivisor)).keys()
+  );
+  const rows = Array.from(Array(Math.floor(gridWidth / rowsDivisor)).keys());
+
+  //placing all elements into grid on page load
+  useEffect(() => {
+    setCoordinatesArray(
+      getGridAllCoordinates(rows.length, columns.length, projectsArrayLength)
+    );
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   const style = {
-    width: `${props.width}px`,
-    height: `${props.height}px`,
+    width: `${gridWidth}px`,
+    height: `${gridHeight}px`,
   };
 
   return (
-    <Container fluid className='grid-container' style={style}>
-      {props.rows.map((_el, i) => {
-        const row = `R${i + 1}`;
-        return (
-          <Row key={row} id={row}>
-            {props.columns.map((_el, j) => {
-              const column = `${row}C${j + 1}`;
-              const animation = `dot-animation${Math.ceil(Math.random() * 7)}`;
-              return (
-                <Col
-                  key={column}
-                  style={{ display: 'flex', justifyContent: 'center' }}>
-                  <div id={column} className='empty box p-3 m-3'>
-                    {props.coordinatesArray &&
-                    props.coordinatesArray.includes(column) ? (
-                      <DotComponent animation={animation} />
-                    ) : (
-                      ''
-                    )}
-                  </div>
-                </Col>
-              );
-            })}
-          </Row>
-        );
-      })}
-    </Container>
+    <>
+      <div
+        className='game'
+        style={{ height: gridHeight + 5, width: gridWidth + 5 }}>
+        <Container fluid className='grid-container' style={style}>
+          {rows.map((_el, i) => {
+            const row = `R${i + 1}`;
+            return (
+              <Row key={row} id={row}>
+                {columns.map((_el, j) => {
+                  const column = `${row}C${j + 1}`;
+                  const animation = `dot-animation${Math.ceil(
+                    Math.random() * 7
+                  )}`;
+                  return (
+                    <Col
+                      key={column}
+                      style={{ display: 'flex', justifyContent: 'center' }}>
+                      <div id={column} className='empty box p-3 m-3'>
+                        {coordinatesArray &&
+                        coordinatesArray.includes(column) ? (
+                          <DotComponent animation={animation} />
+                        ) : (
+                          ''
+                        )}
+                      </div>
+                    </Col>
+                  );
+                })}
+              </Row>
+            );
+          })}
+        </Container>
+      </div>
+    </>
   );
 }
 
